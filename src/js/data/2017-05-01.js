8f7ccf22e2c625f9cd26903012198919fb163d9d
Ebp90 <40249395+Ebp90@users.noreply.github.com>
2023-01-14 14:34:47 -0500

    First

---
+++ src/js/data/2017-05-01.js
@@ -0,0 +1,482 @@
+dataSetVersion = "2022-01-13";
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Generations",
+    key: "stage",
+    tooltip: "Check this to restrict based on the Generation the group debuted in",
+    checked: false,
+    sub: [
+      { name: "Gen 1", key: "st2" },
+      { name: "Gen 2", key: "st2" },
+      { name: "Gen 3", key: "st3" },
+      { name: "Gen 4", key: "st4" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+{
+    name: "AOA",
+    img: "Aoa.jpg",
+    opts: {
+    stage: ["st3"]
+    }
+  },
+{
+    name: "(G)I-DLE",
+    img: "gidle.jpg",
+    opts: {
+    stage: ["st4"]
+    }
+  },
+ {
+     name: "2NE1",
+     img: "2ne1.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+  },
+{
+     name: "TripleS",
+     img: "TripleS.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "Aespa",
+     img: "Aespa.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "Alice",
+     img: "Alice.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+  },
+{
+     name: "Apink",
+     img: "Apink.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+  },
+{
+     name: "April",
+     img: "April.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+  },
+{
+     name: "Billlie",
+     img: "Billlie.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "Blackpink",
+     img: "Blackpink.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+  },
+{
+     name: "Brave Girls",
+     img: "BraveGirls.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+  },
+{
+     name: "Cherry Bullet",
+     img: "CherryBullet.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "CLASS:y",
+     img: "Classy.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "Cignature",
+     img: "Cignature.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "CLC",
+     img: "CLC.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+  },
+{
+     name: "CSR",
+     img: "CSR.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "CRAXY",
+     img: "Craxy.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+  },
+{
+     name: "DIA",
+     img: "Dia.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+  },
+{
+     name: "Dreamcatcher",
+     img: "Dreamcatcher.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "Dreamnote",
+     img: "Dreamnote.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "EVERGLOW",
+     img: "Everglow.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "EXID",
+     img: "EXID.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "f(x)",
+     img: "fx.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+   },
+{
+     name: "FIFTY FIFTY",
+     img: "5050.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "fromis_9",
+     img: "fromis.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "GFRIEND",
+     img: "GFRIEND.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "Girls Day",
+     img: "GirlsDay.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+   },
+{
+     name: "SNSD",
+     img: "SNSD.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+   },
+{
+     name: "gugudan",
+     img: "gugudan.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "H1-KEY",
+     img: "H1key.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "IOI",
+     img: "IOI.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "ICHILLIN",
+     img: "ICHILLIN.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "ily:1",
+     img: "ily1.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "IZ*ONE",
+     img: "izone.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "IVE",
+     img: "IVE.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "ITZY",
+     img: "ITZY.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "KARA",
+     img: "KARA.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+   },
+{
+     name: "Kep1er",
+     img: "kep1er.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "LABOUM",
+     img: "LABOUM.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "Lapillus",
+     img: "Lapillus.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "LE SSERAFIM",
+     img: "lsf.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "LIGHTSUM",
+     img: "LIGHTSUM.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "LOONA",
+     img: "loona.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "Lovelyz",
+     img: "lovelyz.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "MAMAMOO",
+     img: "MAMAMOO.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "miss A",
+     img: "missA.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+   },
+{
+     name: "MOMOLAND",
+     img: "MOMOLAND.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "NATURE",
+     img: "nature.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "NewJeans",
+     img: "newjeans.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "NiziU",
+     img: "niziu.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "NMIXX",
+     img: "nmixx.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "OH MY GIRL",
+     img: "omg.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "Pink Fantasy",
+     img: "pinkfantasy.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "PIXY",
+     img: "pixy.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "PURPLE KISS",
+     img: "pk.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "Red Velvet",
+     img: "rv.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "Rocket Punch",
+     img: "rocketpunch.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "SECRET NUMBER",
+     img: "secretnumber.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "SISTAR",
+     img: "sistar.jpg",
+     opts: {
+     stage: ["st2"]
+     }
+   },
+{
+     name: "STAYC",
+     img: "stayc.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "TRI.BE",
+     img: "tribe.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "TWICE",
+     img: "twice.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "Weeekly",
+     img: "weeekly.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   },
+{
+     name: "Weki Meki",
+     img: "wekimeki.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "WJSN",
+     img: "wjsn.jpg",
+     opts: {
+     stage: ["st3"]
+     }
+   },
+{
+     name: "woo!ah!",
+     img: "wooah.jpg",
+     opts: {
+     stage: ["st4"]
+     }
+   }
+];
